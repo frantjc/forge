ac067bb0ccab153e792f0021646b7e8631ed364f
Frantjc <logsdonalex11@gmail.com>
2022-09-29 22:16:38 -0400

    comments

--- basin.go
+++ basin.go
@@ -6,13 +6,7 @@ import (
 	"io"
 )
 
-// Basin is a cache for Ores and their
-// resulting Metals
-//
-// TODO make more generic; don't interact with Ore or Metal directly
-//      this is because ore.Action changes itself as it runs so it
-//      does not have the same digest before and after a run
-//      and it would be cleaner to get ahold of io interfaces directly
+// Basin is a cache for Ores and their resulting Metals
 type Basin interface {
 	fmt.GoStringer
 
