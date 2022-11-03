5230788dbfa9f45503bf8e2b26cd734ac7e4aab9
Frantjc <logsdonalex11@gmail.com>
2022-11-03 02:59:14 -0400

    really lean into docker cacheing

--- pkg/runtime/container/docker/create_container.go
+++ pkg/runtime/container/docker/create_container.go
@@ -2,71 +2,44 @@ package docker
 
 import (
 	"context"
-	"encoding/json"
-	"errors"
 	"os/exec"
 	"path/filepath"
 	"runtime"
 	"strings"
 
-	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/mount"
 	"github.com/frantjc/forge"
 	"github.com/frantjc/forge/pkg/fn"
 )
 
-var (
-	errNonJSONResponse = errors.New("non-JSON response from image load")
-)
-
 func (d *ContainerRuntime) CreateContainer(ctx context.Context, image forge.Image, config *forge.ContainerConfig) (forge.Container, error) {
-	ilr, err := d.ImageLoad(ctx, image.Blob(), true)
-	if err != nil {
-		return nil, err
-	}
+	// if the Docker daemon already has the image, don't bother loading it in
+	if _, _, err := d.ImageInspectWithRaw(ctx, image.Name()); err != nil {
+		ilr, err := d.ImageLoad(ctx, image.Blob(), true)
+		if err != nil {
+			return nil, err
+		}
 
-	if !ilr.JSON {
-		return nil, errNonJSONResponse
-	}
-
-	// Body typically contains the response
-	//
-	//  '{"stream":"Loaded image[ ID]: <image reference>\n"}'
-	//
-	// Here, we extract it so we can tell the Docker daemon to create
-	// a new container from that image reference
-	m := map[string]string{}
-	if err = json.NewDecoder(ilr.Body).Decode(&m); err != nil {
-		return nil, errNonJSONResponse
-	}
-
-	reference := strings.TrimSpace(
-		strings.TrimPrefix(
-			strings.TrimPrefix(m["stream"], "Loaded image:"), "Loaded image ID:",
-		),
-	)
-
-	defer d.Client.ImageRemove(ctx, reference, types.ImageRemoveOptions{Force: true}) //nolint:errcheck
-
-	if err = ilr.Body.Close(); err != nil {
-		return nil, err
+		if err = ilr.Body.Close(); err != nil {
+			return nil, err
+		}
 	}
 
 	var (
 		addr            = d.Client.DaemonHost()
 		containerConfig = &container.Config{
-			User:        config.User,
-			Env:         config.Env,
-			Cmd:         config.Cmd,
-			WorkingDir:  config.WorkingDir,
-			Entrypoint:  config.Entrypoint,
-			Image:       reference,
-			AttachStdin: true,
-			OpenStdin:   true,
-			StdinOnce:   true,
-			// AttachStdout: true,
-			// AttachStderr: true,
+			User:         config.User,
+			Env:          config.Env,
+			Cmd:          config.Cmd,
+			WorkingDir:   config.WorkingDir,
+			Entrypoint:   config.Entrypoint,
+			Image:        image.Name(),
+			AttachStdin:  true,
+			OpenStdin:    true,
+			StdinOnce:    true,
+			AttachStdout: true,
+			AttachStderr: true,
 		}
 		hostConfig = &container.HostConfig{
 			Privileged: config.Privileged,
@@ -79,7 +52,7 @@ func (d *ContainerRuntime) CreateContainer(ctx context.Context, image forge.Imag
 			Target: "/var/run/docker.sock",
 			Type:   "bind",
 		})
-		containerConfig.Env = append(containerConfig.Env, "DOCKER_HOST="+addr)
+		containerConfig.Env = append(containerConfig.Env, "DOCKER_HOST=/var/run/docker.sock")
 	}
 
 	if runtime.GOOS == "linux" {
