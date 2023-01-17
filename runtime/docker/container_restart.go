0246deb75e182a950522da355299096d77d80156
Frantjc <logsdonalex11@gmail.com>
2023-01-16 23:35:19 -0500

    bump gomod dependencies--docker API had breaking changes so had to account for them

--- runtime/docker/container_restart.go
+++ runtime/docker/container_restart.go
@@ -3,13 +3,17 @@ package docker
 import (
 	"context"
 	"time"
+
+	"github.com/docker/docker/api/types/container"
 )
 
 func (c *Container) Restart(ctx context.Context) error {
-	timeout := time.Minute
+	seconds := -1
 	if deadline, ok := ctx.Deadline(); ok {
-		timeout = time.Until(deadline)
+		seconds = int(time.Until(deadline).Seconds())
 	}
 
-	return c.Client.ContainerRestart(ctx, c.ID, &timeout)
+	return c.Client.ContainerRestart(ctx, c.ID, container.StopOptions{
+		Timeout: &seconds,
+	})
 }
--- runtime/docker/container_stop.go
+++ runtime/docker/container_stop.go
@@ -3,13 +3,17 @@ package docker
 import (
 	"context"
 	"time"
+
+	"github.com/docker/docker/api/types/container"
 )
 
 func (c *Container) Stop(ctx context.Context) error {
-	timeout := time.Minute
+	seconds := -1
 	if deadline, ok := ctx.Deadline(); ok {
-		timeout = time.Until(deadline)
+		seconds = int(time.Until(deadline).Seconds())
 	}
 
-	return c.Client.ContainerStop(ctx, c.ID, &timeout)
+	return c.Client.ContainerStop(ctx, c.ID, container.StopOptions{
+		Timeout: &seconds,
+	})
 }
