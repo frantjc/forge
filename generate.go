d95726e580a0fcbeb82474922f3c72ab9cf3ed46
Frantjc <logsdonalex11@gmail.com>
2023-07-24 16:49:58 -0400

    deps

--- generate.go
+++ generate.go
@@ -3,4 +3,4 @@ package forge
 // Build the shim binary which is often copied into containers
 // and used as their entrypoint, then pack the shim binary
 // so that copy times are as fast as possible.
-//go:generate make fmt lint shims action
+//go:generate make fmt lint shim action
