7713a742accab436bcad35e8d906453a79c7a4ef
Frantjc <logsdonalex11@gmail.com>
2024-04-19 06:44:59 -0400

    test

--- docs/docs.mk
+++ docs/docs.mk
@@ -5,6 +5,6 @@ CWD = $(abspath $(patsubst %/,%,$(dir $(abspath $(lastword $(MAKEFILE_LIST))))))
 INTERMEDIATE = $(CWD)/tmp.gif
 
 gif:
-	@$(TERMINALIZER) render -o=$(INTERMEDIATE) $(CWD)/demo.yml
+	@$(TERMINALIZER) render -o=$(INTERMEDIATE) $(CWD)/gif.yml
 	@$(GIFSICLE) --optimize=3 --colors=256 -o=$(CWD)/demo.gif $(INTERMEDIATE)
 	@rm $(INTERMEDIATE)
