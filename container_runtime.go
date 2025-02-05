c6118a68488693c24c056b9b1ce40e80ad5c5944
Alex <logsdonalex11@gmail.com>
2025-02-05 15:33:53 -0500

    less files

--- container_runtime.go
+++ container_runtime.go
@@ -1,6 +1,62 @@
 package forge
 
-import "context"
+import (
+	"context"
+	"io"
+
+	xslice "github.com/frantjc/x/slice"
+	"github.com/opencontainers/go-digest"
+	imagespecsv1 "github.com/opencontainers/image-spec/specs-go/v1"
+)
+
+type Mount struct {
+	Source      string `json:"source,omitempty"`
+	Destination string `json:"destination,omitempty"`
+}
+
+func overrideMounts(oldMounts []Mount, newMounts ...Mount) []Mount {
+	return append(xslice.Filter(oldMounts, func(m Mount, _ int) bool {
+		return !xslice.Some(newMounts, func(n Mount, _ int) bool {
+			return m.Destination == n.Destination
+		})
+	}), newMounts...)
+}
+
+// ContainerConfig is the configuration that is used to
+// create a container or an exec in a running container.
+type ContainerConfig struct {
+	Entrypoint []string
+	Cmd        []string
+	WorkingDir string
+	Env        []string
+	User       string
+	Privileged bool
+	Mounts     []Mount
+}
+
+// Container represents a container created by a ContainerRuntime.
+type Container interface {
+	GetID() string
+	CopyTo(context.Context, string, io.Reader) error
+	CopyFrom(context.Context, string) (io.ReadCloser, error)
+	Run(context.Context, *Streams) (int, error)
+	Start(context.Context) error
+	Restart(context.Context) error
+	Exec(context.Context, *ContainerConfig, *Streams) (int, error)
+	Stop(context.Context) error
+	Remove(context.Context) error
+	Kill(context.Context) error
+}
+
+// Image represents a image pulled by a ContainerRuntime.
+// Used to create Containers from.
+type Image interface {
+	Manifest() (*imagespecsv1.Manifest, error)
+	Config() (*imagespecsv1.ImageConfig, error)
+	Digest() (digest.Digest, error)
+	Blob() io.Reader
+	Name() string
+}
 
 // ContainerRuntime represents the functionality needed by Runnables
 // to pull OCI images and run containers when being processed.
--- image_refs.go
+++ image_refs.go
@@ -1,22 +1,5 @@
 package forge
 
-import (
-	"io"
-
-	"github.com/opencontainers/go-digest"
-	imagespecsv1 "github.com/opencontainers/image-spec/specs-go/v1"
-)
-
-// Image represents a image pulled by a ContainerRuntime.
-// Used to create Containers from.
-type Image interface {
-	Manifest() (*imagespecsv1.Manifest, error)
-	Config() (*imagespecsv1.ImageConfig, error)
-	Digest() (digest.Digest, error)
-	Blob() io.Reader
-	Name() string
-}
-
 const (
 	DefaultNode10ImageReference = "docker.io/library/node:10"
 	DefaultNode12ImageReference = "docker.io/library/node:12"
