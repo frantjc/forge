3a86cdda1afe880b33538354400b23c7cd83386a
Alex <logsdonalex11@gmail.com>
2025-03-27 16:09:04 -0400

    update shebangs

--- .github/actions/mkdocs/action.sh
+++ .github/actions/mkdocs/action.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
@@ -8,8 +8,10 @@ if [ -f "$REQUIREMENTS" ]; then
     pip install -r "$REQUIREMENTS"
 fi
 
+REPO="https://github.com/$GITHUB_REPOSITORY.git"
+
 if [ -n "$GITHUB_TOKEN" ]; then
-    REMOTE_REPO="https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+    REPO="https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 fi
 
 git config --global user.name "$GITHUB_ACTOR"
@@ -17,7 +19,7 @@ git config --global user.email "$GITHUB_ACTOR@users.noreply.github.com"
 
 mkdocs build --config-file "$GITHUB_WORKSPACE/mkdocs.yml"
 
-git clone --branch=gh-pages --single-branch --depth=1 "$REMOTE_REPO" gh-pages
+git clone --branch=gh-pages --single-branch --depth=1 "$REPO" gh-pages
 cd gh-pages
 
 # remove current content in branch gh-pages
@@ -27,4 +29,4 @@ cp -r ../site/* .
 # commit changes
 git add .
 git commit -m "deploy GitHub Pages"
-git push --force --quiet "$REMOTE_REPO" gh-pages > /dev/null 2>&1
+git push --force --quiet "$REPO" gh-pages > /dev/null 2>&1
